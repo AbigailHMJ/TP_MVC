8e15202b09601d068e4d29f939f6cd7f75fbc5bf
AbigailHMJ <abigail.jean2@gmail.com>
2026-01-20 10:18:37 +0100

    2 - MAJ BDD & CSS

---
+++ config/Database.php
@@ -0,0 +1,13 @@
+<?php 
+
+function getPDO() : PDO{
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname=tp_mvc;charset=utf8mb4;', 'root', '');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $pdo;
+    }
+    catch(PDOException $e){
+        die("Erreur : " . $e->getMessage());
+    }
+}
