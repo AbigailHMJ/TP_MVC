130af5c443663e3f1decffa0dc19f1ca5f5d7c43
AbigailHMJ <abigail.jean2@gmail.com>
2026-01-20 12:10:30 +0100

    3 - test CSS header

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/css/style.css">
+    <script src="./public/javascript/script.js" defer></script>
+    <title>Document</title>
+</head>
+<body>
+   <header>
+    <h1>fika</h1>
+    <subtitle class="subtitle">épicerie suédoise</subtitle>
+   </header>
+   <main></main>
+   <footer></footer>
+</body>
+</html>
